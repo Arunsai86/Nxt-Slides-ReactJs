b1e0710944e4472055193660d88ff9528095c5ca
arunsai <mangarapuarunsai225@gmail.com>
2023-06-26 12:45:58 +0000

    nxt slides

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const Header = () => (
+  <nav>
+    <div className="logo-container">
+      <img
+        className="logo-image"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
+        alt="nxt slides logo"
+      />
+      <h1 className="logo-heading">Nxt Slides</h1>
+    </div>
+  </nav>
+)
+export default Header
